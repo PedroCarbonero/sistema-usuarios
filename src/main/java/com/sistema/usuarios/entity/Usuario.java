6d752af987af7347d3ec4ed110cc69f5f8d9bdcf
Pedro Luis <pedrocarbonero06@gmail.com>
2025-02-08 17:11:52 -0500

    Subir a repositorio

---
+++ src/main/java/com/sistema/usuarios/entity/Usuario.java
@@ -0,0 +1,92 @@
+package com.sistema.usuarios.entity;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String apellido;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private boolean estado;
+
+    public Usuario() {}
+
+    public Usuario(Integer id, String nombre, String apellido, String email, String password, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+        this.estado = estado;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        estado = true;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
